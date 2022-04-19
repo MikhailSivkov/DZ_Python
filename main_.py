b787b3122d2a4491fc9897e7de9852c6cde3a03c
Mikhail <e-cement@bk.ru>
2022-04-19 23:03:34 +0300

    Lesson #1

---
+++ main_.py
@@ -0,0 +1,11 @@
+#from dz5 import new_folders
+#from dz5 import del_folders
+#from dz5 import num_rev
+import dz5
+#import venv.dz5
+
+numbers = [10, 120, 34, 75, 46, 55]
+print(dz5.num_rev([]))
+#new_folders ()
+#del_folders()
+
